2dffd0cf4557bd42e2ec3c9ddd2ceac8592a0e6c
Aiswarya <cuteaisw@gmail.com>
2026-05-30 20:56:50 +0530

    evaluations for  LLM

--- src/config/config.py
+++ src/config/config.py
@@ -9,5 +9,7 @@ load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 SERPER_API_KEY = os.getenv("SERPER_API_KEY")
+LOGFIRE_TOKEN = os.getenv("LOGFIRE_TOKEN")
+JUDGE_GROQ_API_KEY = os.getenv("JUDGE_GROQ_API_KEY")
 
 logger.info("CONGIG INITIALIZED")
